0a395a1601d50a669986fa4705736dc73058bb85
Sari Shehada <sarishehada@gmail.com>
2025-05-14 11:12:31 +0400

    Possible fix to blank page on github pages deployemnt

--- lib/application.dart
+++ lib/application.dart
@@ -5,6 +5,6 @@ class Application extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(body: Center(child: Text('Hola!')));
   }
 }
