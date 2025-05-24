49d6f3d2188c0f35f50a75423bc4656d701227ca
Sari Shehada <sari@Saris-MacBook-Air.local>
2025-05-24 18:50:25 +0400

    Added HomePage

--- lib/application.dart
+++ lib/application.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:sari_shehada_portfolio/pages/home_page.dart';
 
 class Application extends StatelessWidget {
   const Application({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Hola!')));
+    return MaterialApp(home: HomePage());
   }
 }
