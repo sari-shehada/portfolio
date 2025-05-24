1728573c98e385651d2cb7cb0cfa91f7df7af27c
sari-shehada <sari-shehada@users.noreply.github.com>
2025-05-24 15:02:45 +0000

    deploy: 49d6f3d2188c0f35f50a75423bc4656d701227ca

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1910827594"
+    serviceWorkerVersion: "1605324513"
   }
 });
